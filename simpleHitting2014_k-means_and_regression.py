5e3d49cb7c5f0847a7d39ca107402e8ddbcbff3e
anthonywittemann <awittema@usc.edu>
2015-08-06 00:32:26 -0700

    decided to split up the data in load csv function

--- simpleHitting2014_k-means_and_regression.py
+++ simpleHitting2014_k-means_and_regression.py
@@ -5,32 +5,46 @@
 
 """
 import csv
-import math
+import numpy as np
+
 def loadCsv(filename):
-	lines = csv.reader(open(filename, "rb"))
-	dataset = list(lines)
-	dataset = [dataset[x].split(',') for x in range(len(dataset))]
-	players = []
+	dataset = np.array(list(csv.reader(open(filename,"rb"),delimiter=',')))
+	#print dataset
+
+	# arrays to return
+	playerStats = []
+	playerInfo = []
 	statLabels = []
+	infoLabels = []
+	
 	# Rk,Name,Age,Tm,Lg,G,PA,AB,R,H,2B,3B,HR,RBI,SB,CS,BB,SO,BA,OBP,SLG,OPS,OPS+,TB,GDP,HBP,SH,SF,IBB,Pos Summary
+	statIndices = [0,2,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
+	infoIndices = [1,3,4,5,29]
+
 	playerNum = 0
 	for i in range(len(dataset)):
-		if math.isnan( dataset[i][0] ):
-			if dataset[i][0] == 'Rk':
-				statLabels = [str(x) for x in dataset[i]]
-		else:
-			players[playerNum] = [float(x) for x in dataset[i]]
-			players[playerNum][1] = [str(dataset[i][1])] # name
-			players[playerNum][3] = [str(dataset[i][3])] # age
-			players[playerNum][4] = [str(dataset[i][4])] # team
-			players[playerNum][5] = [str(dataset[i][5])] # league
-			players[playerNum][29] = [str(dataset[i][29])] # pos summary
- 			playerNum += 1
+		print dataset[i]
 
-	return players, statLabels
+		if len(dataset[i]) != 0: # skip over blank lines
+
+			# grab the labels
+			if dataset[i][0] == 'Rk' and len(statLabels) == 0:
+				statLabels = [str(x) for x in dataset[i][statIndices]]
+				infoLabels = [str(x) for x in dataset[i][infoIndices]]
+				print 'statLabels {0}'.format(statLabels)
+				print 'infoLabels {0}'.format(infoLabels)
+
+			#grab player info and stats
+			elif dataset[i][0] != 'Rk':
+				playerStats[playerNum] = [float(x) for x in dataset[i][statIndices]]
+				playerInfo[playerNum] = [float(x) for x in dataset[i][infoIndices]]
+ 				playerNum += 1
+		
+
+	return playerStats, statLabels, playerInfo, infoLabels
 
 
 
 filename = 'data/std-batting-2014.csv'
-players, statLabels = loadCsv(filename)
-print('Loaded data file {0} with {1} rows').format(filename, len(dataset))
+playerStats, statLabels, playerInfo, infoLabels = loadCsv(filename)
+print('Loaded data file {0} with {1} players').format(filename, len(playerStats))
